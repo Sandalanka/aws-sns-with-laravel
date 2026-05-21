441e5678c6ae4165f1251d15eab224fcd45a6857
Sandalanka <1415sandalanka@gmail.com>
2026-05-22 05:11:29 +0530

    feat: Differ commit

--- app/Constant/MessageConstant.php
+++ app/Constant/MessageConstant.php
@@ -6,4 +6,5 @@ class MessageConstant
 {
     // General MessageConstant.
     const GENERAL_RESPONSE_ERROR_MESSAGE = 'Something went wrong!';
+    const string SNS_SEND_SUCCESSFULLY = "SNS send successfully.";
 }
